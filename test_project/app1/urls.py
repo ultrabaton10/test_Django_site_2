cc764e2205a56c4af0597211a6c487b235f168fe
ultrabaton10 <ultrabaton10@gmail.com>
2023-06-19 08:36:52 +0300

    Итак, на данном моменте у сайта есть два поля навигации - это все продукты с разными категориями, и продукты с определённой категорией. Пока что, на данном этапе в базе данных присутствует только одна категория, но это пока. Далее я добавлю ещё несколько и буду усложнять этот учебный проект.

--- test_project/app1/urls.py
+++ test_project/app1/urls.py
@@ -7,8 +7,9 @@ from django.conf import settings
 urlpatterns = [
     path('', index, name="index"),
     path('products', products, name="products"),
-    #  re_path('products', products_by_category, name="products_by_category"),
+    # re_path('category/<slug:cat_id>/', products_by_category, name="redirect_to_products_by_chosen_category"),
     path('categories', categories, name="categories"),
-    path('category/<slug:cat_id>/', products_by_category, name="products_by_category"),  #
+    path('category/<slug:cat_id>/', products_by_category, name="products_by_category"),
+    path('create-new-product', ProductCreateView.as_view(), name="create"),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
